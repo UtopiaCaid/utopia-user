eadc51f6012a7886c4686ab79ed47d905c2f0dea
AverySmoothstack <avery.johnson@smoothstack.com>
2021-04-08 14:08:01 -0600

    ensured its working with database variables

--- src/test/java/com/caid/utopia/UtopiaAdminApplicationTests.java
+++ src/test/java/com/caid/utopia/UtopiaAdminApplicationTests.java
@@ -17,8 +17,9 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = UtopiaAdminApplicaton.class)
+@SpringBootTest(classes = UtopiaAdminApplication.class)
 @WebAppConfiguration
 public abstract class UtopiaAdminApplicationTests {
 
