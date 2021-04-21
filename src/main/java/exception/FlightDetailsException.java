f0e018fec14370c35e56ee8cffc904962d09c236
draggling <danielpwoo@gmail.com>
2021-04-21 16:53:19 -0400

    app properties

--- src/main/java/exception/FlightDetailsException.java
+++ src/main/java/exception/FlightDetailsException.java
@@ -10,4 +10,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class FlightDetailsException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8657390405931861036L;
+
 }
